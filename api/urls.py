2bcf12c9e4bf69faffa60e10aa5bd2ecf12fe00b
Renceskieee <quiniano.lp.bsinfotech@gmail.com>
2024-06-04 16:13:56 +0800

    update and delete

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getData),
     path('add/', views.addItem),
+    path('update/<int:pk>/', views.updateItem),
+    path('delete/<int:pk>/', views.deleteItem),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
-# from django.shortcuts import render
+# from django.shortcuts import render, redirect
 from rest_framework.response import Response
+# from django.contrib import messages
 from rest_framework.decorators import api_view
 from api.models import Item
 from .serializers import ItemSerializer
@@ -10,7 +11,6 @@ from .serializers import ItemSerializer
 def getData(request):
     items = Item.objects.all()
     serializer = ItemSerializer(items, many=True)
-
     return Response(serializer.data)
 
 @api_view(['POST'])
@@ -19,3 +19,26 @@ def addItem(request):
     if serializer.is_valid():
         serializer.save()
     return Response(serializer.data)
+
+@api_view(['POST'])
+def updateItem(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(status=404)
+
+    serializer = ItemSerializer(item, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def deleteItem(request, pk):
+    try:
+        item = Item.objects.get(pk=pk)
+    except Item.DoesNotExist:
+        return Response(status=404)
+
+    item.delete()
+    return Response(status=204)
